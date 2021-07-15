d8b7e94d761fbec508ae262b3c63702e5de4cf6a
Felipe da Silva <felipe.ifsc@gmail.com>
2021-07-14 23:03:05 -0300

    Ajustes

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,23 +1,21 @@
 const UserModel = require('../models/user');
 const ObjectId = require('mongoose').Types.ObjectId;
 
-const User = require('../models/user');
-
 const getUser = async (req, res) => {
     const { userId } = req.params;
      
     if (!ObjectId.isValid(userId)) return res.status(400).json( {message: 'Invalid params'} );
     
-
     const user = await UserModel.findById(userId);
 
     return res.json(user);
 }
 
 const createUser = async (req, res) => {
+
     const { name, email, password } = req.body;
 
-    if (await User.findOne({ email }))
+    if (await UserModel.findOne({ email }))
         return res.status(400).send({ error : 'Usuário Já Existe'});
 
     UserModel.create({ name, email, password });
