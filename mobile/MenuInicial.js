ab7164fed177aea76c79e3f6ed1e964e5a64fc49
Jovioluiz <jovioluiz@hotmail.com>
2021-10-17 21:33:40 -0300

    tela cadastro residencia/atendimento/consulta
    tela cadastro de residência/atendimento e tela para visualizar as consultas

--- mobile/MenuInicial.js
+++ mobile/MenuInicial.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View, Button } from 'react-native';
+import {StyleSheet, Text, View, Button, ScrollView, Platform, SafeAreaView } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { DrawerActions, NavigationContainer, useNavigation } from '@react-navigation/native';
-import { RectButton, TextInput } from 'react-native-gesture-handler';
+import { RectButton, TextInput, Input } from 'react-native-gesture-handler';
 import { useState } from "react";
+import { Picker } from '@react-native-picker/picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export function HomeScreen({navigation}){
   return(
@@ -30,122 +32,286 @@ export function CadResidencia({navigation}){
     latitude: '',
     longitude: '',
   });
+
   return(
-    <View style={styles.container}>
-      <Header title="Cadastro de Residência"/>
-        <TextInput
-          style={styles.input}
-          id="cpf"
-          class="form-field"
-          type="number" 
-          placeholder="CPF"
-          name="cpf"
-          keyboardType="number-pad"
-          // value={values.cpf}
-        />
-
-        <TextInput
-          style={styles.input}
-          id="nome"
-          class="form-field"
-          type="text"
-          placeholder="Nome"
-          name="nome"
-          // value={values.nome}
-        />
-
-        <TextInput
-          style={styles.input}
-          id="cep"
-          class="form-field"
-          type="text"
-          placeholder="CEP"
-          name="cep"
-          keyboardType="numbers-and-punctuation"
-          // value={values.cep}
-        />
-
-        <TextInput
-          style={styles.input}
-          id="endereco"
-          class="form-field"
-          type="text"
-          placeholder="Endereço"
-          name="endereco"
-          // value={values.cep}
-        />
-
-        <TextInput
-          style={styles.input}
-          id="numero"
-          class="form-field"
-          type="text"
-          placeholder="Número"
-          name="numero"
-          // value={values.cep}
-        />
-
-        <TextInput
-          style={styles.input}
-          id="complemento"
-          class="form-field"
-          type="text"
-          placeholder="Complemento"
-          name="complemento"
-          // value={values.cep}
-        />
-
-        <TextInput
-          style={styles.input}
-          id="latitude"
-          class="form-field"
-          type="number"
-          placeholder="Latitude"
-          name="latitude"
-          keyboardType="number-pad"
-          // value={values.cep}
-        />
-
-        <TextInput
-          style={styles.input}
-          id="longitude"
-          class="form-field"
-          type="number"
-          placeholder="Longitude"
-          name="longitude"
-          keyboardType="number-pad"
-          // value={values.cep}
-        />
-
-        <View style={styles.button}>
-          <Button 
-            title="Gravar"
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.container}>
+        <Header title="Cadastro de Residência"/>
+          <TextInput
+            style={styles.input}
+            id="cpf"
+            class="form-field"
+            type="number" 
+            placeholder="CPF"
+            name="cpf"
+            keyboardType="number-pad"
+            value={values.cpf}
+            onChangeText={text=>setValues(text)}
           />
+
+          <TextInput
+            style={styles.input}
+            id="nome"
+            class="form-field"
+            type="text"
+            placeholder="Nome"
+            name="nome"
+            value={values.nome}
+            onChangeText={text=>setValues(text)}
+          />
+
+          <TextInput
+            style={styles.input}
+            id="cep"
+            class="form-field"
+            type="text"
+            placeholder="CEP"
+            name="cep"
+            keyboardType="numbers-and-punctuation"
+            value={values.cep}
+            onChangeText={text=>setValues(text)}
+          />
+
+          <TextInput
+            style={styles.input}
+            id="endereco"
+            class="form-field"
+            type="text"
+            placeholder="Endereço"
+            name="endereco"
+            value={values.endereco}
+            onChangeText={text=>setValues(text)}
+          />
+
+          <TextInput
+            style={styles.input}
+            id="numero"
+            class="form-field"
+            type="number"
+            placeholder="Número"
+            name="numero"
+            keyboardType="number-pad"
+            value={values.numero}
+            onChangeText={text=>setValues(text)}
+          />
+
+          <TextInput
+            style={styles.input}
+            id="complemento"
+            class="form-field"
+            type="text"
+            placeholder="Complemento"
+            name="complemento"
+            value={values.complemento}
+            onChangeText={text=>setValues(text)}
+          />
+
+          <TextInput
+            style={styles.input}
+            id="latitude"
+            class="form-field"
+            type="number"
+            placeholder="Latitude"
+            name="latitude"
+            keyboardType="number-pad"
+            value={values.latitude}
+            onChangeText={text=>setValues(text)}
+          />
+
+          <TextInput
+            style={styles.input}
+            id="longitude"
+            class="form-field"
+            type="number"
+            placeholder="Longitude"
+            name="longitude"
+            keyboardType="number-pad"
+            value={values.longitude}
+            onChangeText={text=>setValues(text)}
+          />
+
+          <View style={styles.button}>
+            <Button 
+              title="Gravar"
+            />
+          </View>
         </View>
 
-      <View style={styles.content}>
-      </View>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export function RegAtendimento({navigation}){
+  const [values, setValues] = useState({
+    cpfUsuario: '',
+    idDoenca: '',
+    descricao: '',
+    dtRetornoEstipulado: '',
+  });
+
+  const [niveis, setNiveis] = useState({
+    baixo: '',
+    normal: '',
+    alto: '',
+  });
+
+const [isPickerShow, setIsPickerShow] = useState(false);
+const [date, setDate] = useState(new Date());
+
+const showPicker = () => {
+  setIsPickerShow(true);
+};
+
+const onChange = (event, selectedDate) => {
+  const currentDate = selectedDate || date;
+  if (Platform.OS === 'android'){
+    setIsPickerShow(true);
+  }
+  setDate(currentDate);
+};
+
   return(
-    <View style={styles.container}>
-      <Header title="Registrar Atendimento"/>
-      <View style={styles.content}>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <ScrollView>
+        <View style={styles.container}>
+          <Header title="Registrar Atendimento"/>
+            <TextInput
+                style={styles.input}
+                id="cpf_usuario"
+                class="form-field"
+                type="number"
+                placeholder="CPF Usuário"
+                name="cpf_usuario"
+                keyboardType="number-pad"
+                value={values.cpfUsuario}
+                onChangeText={text=>setValues(text)}
+              />
+
+              <Text style={{margin:12}}> Niveis de Infestação</Text>
+              <Picker
+                selectedValue={niveis}
+                style={{height: 40, width: 200}}
+                onValueChange={(itemValue, itemIndex) => setNiveis(itemValue)}
+              >
+                <Picker.Item label="Baixo" value="baixo"/>
+                <Picker.Item label="Médio" value="medio"/>
+                <Picker.Item label="Alto" value="alto"/>
+              </Picker>
+
+              <TextInput
+                style={styles.inputDescricao}
+                id="descricao"
+                class="form-field"
+                type="text"
+                placeholder="Descrição"
+                name="descricao"
+                value={values.descricao}
+                onChangeText={text=>setValues(text)}
+              />
+
+            {isPickerShow && (
+                <DateTimePicker
+                  value={date}
+                  mode={'date'}
+                  display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                  is24Hour={true}
+                  onChange={onChange}
+                  style={styles.datePicker}
+                />
+              )}
+
+                {/* <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  is24Hour={true}
+                  display="default"
+                  onChange={onChange}
+               /> */}
+
+              <View style={styles.button}>
+                <Button 
+                  title="Gravar"
+                />
+              </View>
+
+              {/* <DatePicker
+                style={{width: 200}}
+                // date={this.state.date}
+                format="YYYY-MM-DD"
+                minDate="10-07-2019"
+                maxDate="31-08-2019"
+                onDateChange={this.selectDate}
+              />    */}
+              {/* <Text style={{fontSize: 25}} >{this.state.date}</Text>     */}
+
+              {/* <TextInput 
+                style={styles.input}
+                id="data_retorno"
+                class="form-field"
+                type="date"
+                format="DD-MM-YYYY"
+                name="data_retorno"
+                value={values.dtRetornoEstipulado}
+                onChangeText={text=>setValues(text)}
+              /> */}
+        </View>
+    </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export function HistoricoAtendimento({navigation}){
+  const [filtros, setFiltros] = useState({
+    usuario: '',
+    tpfoco: '',
+    residencia: '',
+  });
+
+  const [filtro, setFiltro]=useState({
+    pesquisa: ''
+  })
+
   return(
-    <View style={styles.container}>
-      <Header title="Histórico Atendimento"/>
-      <View style={styles.content}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.container}>
+          <Header title="Histórico Atendimento"/>
+          
+          <Text style={{margin:12}}> Filtrar por:</Text>
+              <Picker
+                selectedValue={filtros}
+                style={{height: 40, width: 200}}
+                onValueChange={(itemValue, itemIndex) => setFiltros(itemValue)}
+              >
+                <Picker.Item label="Usuário" value="usuario"/>
+                <Picker.Item label="Tipo de Foco" value="tpfoco"/>
+                <Picker.Item label="Residência" value="residencia"/>
+              </Picker>
+
+          <TextInput
+            style={styles.input}
+            id="pesquisa"
+            class="form-field"
+            type="text"
+            placeholder="Digite o Texto"
+            name="pesquisa"
+            value={filtro.pesquisa}
+            onChangeText={text=>setFiltro(text)}
+          />
+
+          <View style={styles.button}>
+                <Button 
+                  title="Pesquisar"
+                />
+          </View>
+
       </View>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
+
+
   )
 }
 
@@ -179,6 +345,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    margin:1,
+    marginBottom:9,
+    paddingBottom:50
   },
   content:{
     flex: 1,
@@ -215,11 +387,21 @@ const styles = StyleSheet.create({
   },
 
   input:{
-        height: 40,
+    height: 40,
     margin: 12,
     borderWidth: 2,
     padding: 10,
     borderRadius:5
-  }
+  },
+
+  inputDescricao:{
+    height: 150,
+    margin: 12,
+    borderWidth: 2,
+    padding: 10,
+    borderRadius:5,
+    textAlignVertical: 'top',
+    
+  },
   
 });
